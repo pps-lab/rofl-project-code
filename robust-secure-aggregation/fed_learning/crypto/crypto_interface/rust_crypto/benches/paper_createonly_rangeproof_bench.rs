373e2170587cd676aa1124e5d487199ecb145a64
Hidde L <mail.hidde@gmail.com>
2020-10-09 14:48:50 +0200

    revert

--- robust-secure-aggregation/fed_learning/crypto/crypto_interface/rust_crypto/benches/paper_createonly_rangeproof_bench.rs
+++ robust-secure-aggregation/fed_learning/crypto/crypto_interface/rust_crypto/benches/paper_createonly_rangeproof_bench.rs
@@ -34,10 +34,10 @@ use std::time::{Duration, Instant};
 
 use std::thread::sleep;
 
-static DIM: [usize; 1] = [524288];
+static DIM: [usize; 4] = [32768, 131072, 262144, 524288];
 // static DIM: [usize; 4] = [25000, 100000, 250000, 500000];
 static RANGE: [usize; 1] = [8];
-static N_PARTITION: usize = 4;
+static N_PARTITION: usize = 2;
 static num_samples: usize = 4;
 
 fn paper_createonly_rangeproof_bench_fn(bench: &mut Bencher) {
