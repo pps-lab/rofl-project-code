02d278fc78922657c59ce00794fb0910eb80926a
Hidde L <mail.hidde@gmail.com>
2020-10-12 16:50:54 +0200

    Change schedule

--- robust-secure-aggregation/fed_learning/client/util/lr_scheduler.py
+++ robust-secure-aggregation/fed_learning/client/util/lr_scheduler.py
@@ -31,11 +31,11 @@ def lr_schedule(self, epoch):
     lr = 1e-3 # federated
     if epoch > 180:
         lr *= 0.5e-3
-    elif epoch > 160:
+    elif epoch > 60:
         lr *= 1e-3
-    elif epoch > 120:
+    elif epoch > 40:
         lr *= 1e-2
-    elif epoch > 80:
+    elif epoch > 20:
         lr *= 1e-1
     print('Learning rate: ', lr)
     return lr
