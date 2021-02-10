cc9ed4fc097776c573a1931b41c7ddc527810ed9
Hidde L <mail.hidde@gmail.com>
2021-02-10 11:09:23 +0100

    Update adversarial-framework

--- adversarial-framework/scripts/compare_optimization_allcnn.sh
+++
@@ -1,7 +0,0 @@
-
-# Compare if our PGD algorithm works better than basic training and clipping at the end.
-
-./cil-train.sh exp_cifar_allcnn -c ./train_configs/cifar_allcnn.yml
-
-./cil-train.sh exp_cifar_allcnn_randommask_25 -c ./train_configs/cifar_allcnn.yml --federated_dropout_rate 0.25
-./cil-train.sh exp_cifar_allcnn_subspace_25 -c ./train_configs/cifar_allcnn.yml --model_name allcnn_intrinsic --intrinsic_dimension 325000
