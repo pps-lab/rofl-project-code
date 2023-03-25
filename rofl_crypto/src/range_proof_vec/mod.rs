100ee70365e24264d3f655493ffb525778b93b9c
Hidde L <mail.hidde@gmail.com>
2023-03-24 23:17:16 -0400

    Use the same BulletproofGens for each thread for speed and memory

--- rofl_crypto/src/range_proof_vec/mod.rs
+++ rofl_crypto/src/range_proof_vec/mod.rs
@@ -42,8 +42,6 @@ pub fn create_rangeproof(
         .map(|x| read_from_bytes(&x))
         .collect();
 
-    println!(" BP 2 convert...");
-
     // extend vector length to pow2
     let value_fp_vec_shifted_clipped_ext: Vec<u64> =
         extend_vec_to_pow2(&value_fp_vec_clipped_shifted, 0)
@@ -51,7 +49,6 @@ pub fn create_rangeproof(
             .map(|x| *x as u64)
             .collect();
     let blinding_vec_ext: Vec<Scalar> = extend_vec_to_pow2(&blinding_vec, Scalar::zero());
-    println!(" BP 3 convert...");
 
     // chunk the vector for par_iter
     let n_chunks: usize = std::cmp::min(value_fp_vec_shifted_clipped_ext.len(), n_partition);
@@ -60,30 +57,27 @@ pub fn create_rangeproof(
         .chunks(chunk_size)
         .map(|x| x.to_vec())
         .collect();
-    println!(" BP 4 convert...");
 
     let blinding_fp_vec_chunks: Vec<Vec<Scalar>> = blinding_vec_ext
         .chunks(chunk_size)
         .map(|x| x.to_vec())
         .collect();
-    println!(" BP 5 convert...");
 
     let proof_args: Vec<(&Vec<u64>, &Vec<Scalar>)> = value_fp_vec_chunks
         .iter()
         .zip(&blinding_fp_vec_chunks)
         .collect();
-    println!(" BP 6 convert...");
 
     let pc_gens = PedersenGens::default();
+    // println
+    let bp_gens = BulletproofGens::new(prove_range, chunk_size);
     let res_vec: Vec<Result<(RangeProof, Vec<CompressedRistretto>), ProofError>> = proof_args
         .par_iter()
-        .map(|(v, b)| create_rangeproof_helper(v, b, prove_range, &pc_gens))
+        .map(|(v, b)| create_rangeproof_helper(v, b, prove_range, &pc_gens, &bp_gens))
         .collect();
-    println!(" BP 7 convert...");
 
     // bundle up results
     let mut res_range_proof_vec: Vec<RangeProof> = Vec::with_capacity(n_chunks);
-    println!(" BP 8 convert...");
 
     let mut res_commit_vec: Vec<CompressedRistretto> =
         Vec::with_capacity(value_fp_vec_shifted_clipped_ext.len());
@@ -96,14 +90,12 @@ pub fn create_rangeproof(
             Err(e) => return Err(e.into()),
         }
     }
-    println!(" BP 9 convert...");
 
     // downshift values
     let rp_vec_shifted = crp_to_rp_vec(&res_commit_vec);
     let inv_offset_rp: RistrettoPoint = pc_gens.commit(-offset_value_scalar, Scalar::zero());
     let rp_vec =
         compute_shifted_values_rp(&rp_vec_shifted[0..value_vec_clipped.len()], &inv_offset_rp);
-    println!(" BP 10 convert...");
 
     Ok((res_range_proof_vec, rp_vec))
 }
@@ -127,13 +119,11 @@ fn create_rangeproof_helper(
     blinding_vec: &[Scalar],
     prove_range: usize,
     pc_gens: &PedersenGens,
+    bp_gens: &BulletproofGens,
 ) -> Result<(RangeProof, Vec<CompressedRistretto>), ProofError> {
-    println!(" BP gens...");
-    let bp_gens = BulletproofGens::new(prove_range, value_vec.len());
     let mut transcript = Transcript::new(b"RangeProof");
-    println!(" BP 1 proving range...");
     match RangeProof::prove_multiple(
-        &bp_gens,
+        bp_gens,
         &pc_gens,
         &mut transcript,
         &value_vec,
@@ -182,9 +172,10 @@ pub fn verify_rangeproof(
         .zip(&commit_vec_vec_shifted)
         .collect();
 
+    let bp_gens = BulletproofGens::new(prove_range, commit_vec.len());
     let res_vec: Vec<Result<bool, ProofError>> = verify_args
         .par_iter()
-        .map(|(rp, crp_chunk)| verify_rangeproof_helper(rp, crp_chunk, prove_range, &pc_gens))
+        .map(|(rp, crp_chunk)| verify_rangeproof_helper(rp, crp_chunk, prove_range, &pc_gens, &bp_gens))
         .collect();
 
     let mut res: bool = true;
@@ -202,13 +193,13 @@ pub fn verify_rangeproof_helper(
     commit_vec: &Vec<CompressedRistretto>,
     range_exp: usize,
     pc_gens: &PedersenGens,
+    bp_gens: &BulletproofGens,
 ) -> Result<bool, ProofError> {
-    let bp_gens = BulletproofGens::new(64, commit_vec.len());
     let verify_range: usize = range_exp;
     let mut transcript = Transcript::new(b"RangeProof");
 
     match range_proof.verify_multiple(
-        &bp_gens,
+        bp_gens,
         &pc_gens,
         &mut transcript,
         &commit_vec,
